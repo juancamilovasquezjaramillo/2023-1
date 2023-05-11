2bf74bf7709e65546b076a7b7bb1371afd0216c1
juancamilovasquezjaramillo <110747214+juancamilovasquezjaramillo@users.noreply.github.com>
2023-05-11 13:23:58 -0500

    Update BigInteger.h

--- proyecto/BigInteger.h
+++ proyecto/BigInteger.h
@@ -9,8 +9,38 @@ private:
     std::vector<int> digits;
 
 public:
+    BigInteger();
     BigInteger(const std::string& numberString);
     BigInteger(const BigInteger& other);
+    
+    BigInteger add(const BigInteger& other) const;
+    BigInteger product(const BigInteger& other) const;
+    BigInteger subtract(const BigInteger& other) const;
+    BigInteger quotient(const BigInteger& other) const;
+    BigInteger remainder(const BigInteger& other) const;
+    BigInteger pow(const BigInteger& exponent) const;
+
+    std::string toString() const;
+
+    BigInteger operator+(const BigInteger& other) const;
+    BigInteger operator-(const BigInteger& other) const;
+    BigInteger operator*(const BigInteger& other) const;
+    BigInteger operator/(const BigInteger& other) const;
+    BigInteger operator%(const BigInteger& other) const;
+    bool operator==(const BigInteger& other) const;
+    bool operator<(const BigInteger& other) const;
+    bool operator<=(const BigInteger& other) const;
+
+    static BigInteger sumarListaValores(const std::vector<BigInteger>& values);
+    static BigInteger multiplicarListaValores(const std::vector<BigInteger>& values);
+
+private:
+    std::vector<int> digits_;
+
+    void normalize();
+};
+
+#endif
 
 };
 
