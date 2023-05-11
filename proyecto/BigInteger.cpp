1e82a9f8c6f058744648e616bba104afa6ea2c09
juancamilovasquezjaramillo <110747214+juancamilovasquezjaramillo@users.noreply.github.com>
2023-05-11 13:25:50 -0500

    Update BigInteger.cpp

--- proyecto/BigInteger.cpp
+++ proyecto/BigInteger.cpp
@@ -1,16 +1,1 @@
 #include "BigInteger.h"
-
-
-BigInteger::BigInteger(const std::string& numberString) {
-    for (char c : numberString) {
-        if ((c)) {
-            digits.push_back(c - '0');
-        }
-    }
-}
-
-BigInteger::BigInteger(const BigInteger& other) {
-    digits = other.digits;
-}
-
-// aun me falta implementar mas detalles
