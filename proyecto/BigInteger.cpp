c2a3db8f48b3d00e0d67d35d42259a46d8bbbbac
juancamilovasquezjaramillo <110747214+juancamilovasquezjaramillo@users.noreply.github.com>
2023-05-13 18:08:39 -0500

    Update BigInteger.cpp

--- proyecto/BigInteger.cpp
+++ proyecto/BigInteger.cpp
@@ -1,1 +1,14 @@
 #include "BigInteger.h"
+
+
+BigInteger::BigInteger(std::string& numberString) {
+    for (char c : numberString) {
+        if ((c)) {
+            digits.push_back(c - '0');
+        }
+    }
+}
+
+BigInteger::BigInteger(BigInteger& other) {
+    digits = other.digits;
+}
